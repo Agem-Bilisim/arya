8bfdc34a25e59529a44e268a55f091619f45091d
cem.gungor <cemm.gungor@gmail.com>
2015-10-09 12:33:58 +0300

    fixed parent-child hierarchy of android components

--- arya-interpreter-android/src/main/java/tr/com/agem/arya/interpreter/parser/AryaMetadataParser.java
+++ arya-interpreter-android/src/main/java/tr/com/agem/arya/interpreter/parser/AryaMetadataParser.java
@@ -1,5 +1,8 @@
 package tr.com.agem.arya.interpreter.parser;
 
+import android.view.View;
+import android.view.ViewGroup;
+
 import org.xml.sax.Attributes;
 import org.xml.sax.helpers.DefaultHandler;
 
@@ -9,6 +12,7 @@ import java.util.Stack;
 import tr.com.agem.arya.interpreter.components.AryaComboItem;
 import tr.com.agem.arya.interpreter.components.AryaListCell;
 import tr.com.agem.arya.interpreter.components.AryaListItem;
+import tr.com.agem.arya.interpreter.components.AryaRadio;
 import tr.com.agem.arya.interpreter.components.base.AryaMain;
 import tr.com.agem.arya.interpreter.components.menu.AryaMenuItem;
 import tr.com.agem.arya.interpreter.components.AryaMultipleComboItem;
@@ -33,7 +37,8 @@ public class AryaMetadataParser extends DefaultHandler {
         
         if (comp != null) {
 
-            if(comp instanceof AryaComboItem || comp instanceof AryaMultipleComboItem || comp instanceof AryaListItem || comp instanceof AryaListCell|| comp instanceof AryaMenuItem ){
+            if(currentComponent.size() > 0){
+                ((ViewGroup)((View)comp).getParent()).removeView((View)comp);
                 comp.setComponentParent(currentComponent.peek());
             }
             else {
